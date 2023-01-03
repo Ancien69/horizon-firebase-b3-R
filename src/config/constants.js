11bbff9f491fc94ccba929677ef7be49c6992a73
Stephane HENRY <stephane.henry123@gmail.com>
2023-01-03 15:52:51 +0100

    first commit

---
+++ src/config/constants.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDs6O9gtmgMH6kSPh4RxD-n2qAV78DSYcw',
+  authDomain: 'horizon-backend-isitech-b3-r.firebaseapp.com',
+  projectId: 'horizon-backend-isitech-b3-r',
+  storageBucket: 'horizon-backend-isitech-b3-r.appspot.com',
+  messagingSenderId: '298433815776',
+  appId: '1:298433815776:web:cc00d9913f4edc45d0dc85',
+  measurementId: 'G-W4K113FS4H'
+};
